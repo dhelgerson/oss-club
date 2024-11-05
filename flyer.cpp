2d888a52644bbadb5bacf980593b22d1ab7aa4e4
Drew Helgerson <tah568@msstate.edu>
2024-11-05 10:30:22 -0600

    added ascii terminal flyer

---
+++ flyer.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main(){
+
+
+cout << "\x1b[1;36m                   -`                 \033[0m     _             _    " << endl; 
+cout << "\x1b[1;36m                  .o+`                \033[0m    / \\   _ __ ___| |__  " << endl; 
+cout << "\x1b[1;36m                 `ooo/                \033[0m   / _ \\ | '__/ __| '_ \\ " << endl; 
+cout << "\x1b[1;36m                `+oooo:               \033[0m  / ___ \\| | | (__| | | |" << endl; 
+cout << "\x1b[1;36m               `+oooooo:              \033[0m /_/   \\_\\_|  \\___|_| |_|" << endl; 
+cout << "\x1b[1;36m               -+oooooo+:             \033[0m " << endl; 
+cout << "\x1b[1;36m             `/:-:++oooo+:            \033[0m  _     _                  " << endl; 
+cout << "\x1b[1;36m            `/++++/+++++++:           \033[0m | |   (_)_ __  _   ___  __" << endl; 
+cout << "\x1b[1;36m           `/++++++++++++++:          \033[0m | |   | | '_ \\| | | \\ \\/ /" << endl; 
+cout << "\x1b[1;36m          `/+++ooooooooooooo/`        \033[0m | |___| | | | | |_| |>  <" << endl; 
+cout << "\x1b[1;36m         ./ooosssso++osssssso+`       \033[0m |_____|_|_| |_|\\__,_/_/\\_\\" << endl; 
+cout << "\x1b[1;36m        .oossssso-````/ossssss+`      \033[0m " << endl; 
+cout << "\x1b[1;36m       -osssssso.      :ssssssso.     \033[0m __        __         _        _" << endl; 
+cout << "\x1b[1;36m      :osssssss/        osssso+++.    \033[0m \\ \\      / /__  _ __| | _____| |__   ___  _ __" << endl; 
+cout << "\x1b[1;36m     /ossssssss/        +ssssooo/-    \033[0m  \\ \\ /\\ / / _ \\| '__| |/ / __| '_ \\ / _ \\| '_ \\" << endl; 
+cout << "\x1b[1;36m   `/ossssso+/:-        -:/+osssso+-  \033[0m   \\ V  V / (_) | |  |   <\\__ \\ | | | (_) | |_) |" << endl; 
+cout << "\x1b[1;36m  `+sso+:-`                 `.-/+oso: \033[0m    \\_/\\_/ \\___/|_|  |_|\\_\\___/_| |_|\\___/| .__/" << endl; 
+cout << "\x1b[1;36m `++:.                           `-/+/\033[0m                                          |_|  " << endl; 
+cout << "\x1b[1;36m .`                                 ` \033[0m " << endl; 
+cout << "" << endl;
+cout << "Join Us for a live ArchLinux Installation Workshop" << endl; 
+cout << "" << endl; 
+cout << "	- Wednesday November 6th, 2024" << endl; 
+cout << "	- Live Arch Linux Installation" << endl; 
+cout << "	- Overview of Storage schemes and window managers" << endl; 
+cout << "	- Free Pizza" << endl; 
+cout << "	- Comradery" << endl; 
+cout << "" << endl; 
+cout << "               _________________________" << endl; 
+cout << "              < Wouldn't wanna miss it! >" << endl; 
+cout << "               -------------------------" << endl; 
+cout << "                                      \\   ^__^" << endl; 
+cout << "                                       \\  (@@)\\_______" << endl; 
+cout << "                                          (__)\\       )\\/\\\\" << endl; 
+cout << "                                              ||----w |" << endl; 
+cout << "                                              ||     ||" << endl; 
+
+}
